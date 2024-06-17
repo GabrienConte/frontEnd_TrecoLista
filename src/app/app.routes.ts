b9ccef253a049696d868dff2f678892dbd8d6037
GabrienConte <gabrielconte0@gmail.com>
2024-06-16 22:48:50 -0300

    feat: adiciona rotiamento para componente de cria produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { ListComponent } from './feature/list/list.component';
+import { CreateProductComponent } from './feature/create-product/create-product.component';
 
-export const routes: Routes = [{
-    path: '',
-    component: ListComponent
-}];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    },
+    {
+        path: 'create-product',
+        component: CreateProductComponent
+    }
+];
