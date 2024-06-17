b38e3fdba39bdccb00520ab726b0631ffe9592a5
GabrienConte <gabrielconte0@gmail.com>
2024-06-16 22:38:48 -0300

    feat: adiciona link para criar um produto

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -3,12 +3,16 @@ import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
