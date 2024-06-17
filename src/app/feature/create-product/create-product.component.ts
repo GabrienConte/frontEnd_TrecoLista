0b6cf6d6835155eb457b52e167b6cf2606ad8771
GabrienConte <gabrielconte0@gmail.com>
2024-06-16 23:24:41 -0300

    feature: cria formulario

--- src/app/feature/create-product/create-product.component.ts
+++ src/app/feature/create-product/create-product.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
 export class CreateProductComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true, 
+      validators: Validators.required
+    }),
+  });
 
+  onSubmit() {
+    this.form.controls.title.value;
+  }
 }
