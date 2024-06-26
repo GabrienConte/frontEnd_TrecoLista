31fb57d115b11abab0fe3980333ce74cfcaf8e5a
GabrienConte <gabrielconte0@gmail.com>
2024-06-26 17:27:59 -0300

    ajuste na tarefa de listagem

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
@@ -8,23 +8,60 @@ import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { CardProductComponent } from '../../shared/components/card-product/card-product.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule, MatDividerModule, MatIconModule, CardProductComponent],
+  imports: [
+    CardComponent, 
+    RouterLink, 
+    MatButtonModule, 
+    MatDividerModule,
+    MatIconModule, 
+    CardProductComponent,
+    MatFormFieldModule,
+    MatInputModule
+    ],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  @Input() variant: 'otherProdutos' | 'myProdutos' | 'allProdutos' = 'allProdutos'
   products: Product[] = [];
 
   productsService = inject(ProductsService);
 
+  searchProducts(event: Event) {
+    const input = event.target as HTMLInputElement;
+    // Lógica para buscar produtos
+  }
+
+  scrollLeft() {
+    const container = document.querySelector('.product-list');
+    container?.scrollBy({ left: -200, behavior: 'smooth' });
+  }
+
+  scrollRight() {
+    const container = document.querySelector('.product-list');
+    container?.scrollBy({ left: 200, behavior: 'smooth' });
+  }
+
   ngOnInit() {
-    this.productsService.getAll().subscribe((products) => {
-      this.products = products
-    })
+    switch (this.variant) {
+      case 'allProdutos':
+        this.productsService.getAll().subscribe((products) => {
+          this.products = products
+        });
+      break;
+      case 'myProdutos':
+
+      break;
+      case 'otherProdutos':
+
+      break;
+    }
   }
 }
