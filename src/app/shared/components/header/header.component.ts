fb5fb9eaa9137a3f65e5eccf8f931894d77c7b88
GabrienConte <gabrielconte0@gmail.com>
2024-06-26 17:27:37 -0300

    ajuste header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule } from "@angular/material/toolbar"
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule, 
+    MatToolbarModule, 
+    MatIconModule, 
+    MatButtonModule, 
+    RouterModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
