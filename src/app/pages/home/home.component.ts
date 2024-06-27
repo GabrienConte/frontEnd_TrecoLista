22baf5c89a0ccf7dc9e6ce0d4b44c855df582e6d
GabrienConte <gabrielconte0@gmail.com>
2024-06-27 11:18:32 -0300

    ajuste home para possuir o botão de adicionar produto e o campo de pesquisar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../shared/components/container/container.component';
 import { ListComponent } from '../../feature/list/list.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ContainerComponent, ListComponent],
+  imports: [
+    ContainerComponent, 
+    ListComponent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule, 
+    MatDividerModule,
+    MatIconModule, 
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
+  searchProducts(event: Event) {
+    const input = event.target as HTMLInputElement;
+    // Lógica para buscar produtos
+  }
 }
