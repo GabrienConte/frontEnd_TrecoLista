735b724b840940538dd66108f41329a5266f8ae7
GabrienConte <gabrielconte0@gmail.com>
2024-06-27 11:17:53 -0300

    adiciona menu no botão do usuário

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatMenuModule} from '@angular/material/menu';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +14,9 @@ import { CommonModule } from '@angular/common';
     CommonModule, 
     MatToolbarModule, 
     MatIconModule, 
-    MatButtonModule, 
+    MatButtonModule,
+    MatMenuModule, 
+    MatDividerModule,
     RouterModule
   ],
   templateUrl: './header.component.html',
